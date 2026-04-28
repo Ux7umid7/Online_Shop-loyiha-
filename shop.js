02fc15a14490de6018ae31d0e1f1831ed87f3a19
Ux7umid7 <ux7umidjon7@gmail.com>
2026-04-28 16:32:48 +0500

    gotopshop

--- shop.js
+++ shop.js
@@ -118,4 +118,20 @@ function render(array) {
     });
 }
 
+let scrollBtn = document.querySelector(".scroll-top");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+        scrollBtn.classList.add("show");
+    } else {
+        scrollBtn.classList.remove("show");
+    }
+});
+
+scrollBtn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
 render(products)
